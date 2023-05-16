d13e4492e128f3655374a7481503bb4db8b15f1c
Julien Vincent <m@julienvincent.io>
2023-05-16 18:36:21 +0100

    Remove old config code

--- lua/plugins/disable.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "stevearc/dressing.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "goolord/alpha-nvim", enabled = false },
-}
