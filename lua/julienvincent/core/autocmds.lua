f7f45594091b8e5312f4093573cd6e538a827506
Julien Vincent <m@julienvincent.io>
2023-05-18 10:59:52 +0100

    Experimental gruvbox diff highlights

--- lua/julienvincent/core/autocmds.lua
+++ lua/julienvincent/core/autocmds.lua
@@ -41,3 +41,20 @@ autocmd("BufNewFile", {
     vim.diagnostic.disable(event.buf)
   end,
 })
+
+-- local colors = require("gruvbox.palette").colors;
+--
+-- function FixGruvbox()
+--   vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { bg = "#431313" })
+--   vim.api.nvim_set_hl(0, 'DiffDelete', { bg = "none", fg = colors.dark2 })
+--   vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = "none", fg = colors.dark2 })
+--   vim.api.nvim_set_hl(0, 'DiffAdd', { bg = "#142a03" })
+--   vim.api.nvim_set_hl(0, 'DiffChange', { bg = "#3B3307" })
+--   vim.api.nvim_set_hl(0, 'DiffText', { bg = "#4D520D" })
+-- end
+-- FixGruvbox()
+--
+-- vim.api.nvim_create_autocmd(
+--   "ColorScheme",
+--     { pattern = { "gruvbox" }, callback = FixGruvbox }
+-- )
