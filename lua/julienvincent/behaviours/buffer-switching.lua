5e82f6679448fe143be3bfa8aa30aa14c20e1583
Julien Vincent <m@julienvincent.io>
2023-05-18 11:32:31 +0100

    Improve buffer-tabbing buf tracking logic

--- lua/julienvincent/behaviours/buffer-switching.lua
+++ lua/julienvincent/behaviours/buffer-switching.lua
@@ -1,21 +1,53 @@
 local M = {}
 
--- Initialize an empty table to store previous buffers for each window
+local MAX_DEPTH = 20
+
+-- Keeps a 'stack' of previously visited buffers for each window. This is stored as a stack as intermediary
+-- buffers are opened all the time during normal development. As they are closed, the are popped off the
+-- stack.
+--
+-- When switching to the previous buffer for a window, we just pop the last buffer off the stack
 local PREV_BUFS = {}
 
--- Function to switch to the previous buffer
-function switch_to_prev_buf()
-  local win = vim.api.nvim_get_current_win()
-  local buf = vim.api.nvim_win_get_buf(win)
+-- local function remove_buf_from_stack(stack, val)
+--   for i, v in ipairs(stack) do
+--     if v == val then
+--       table.remove(stack, i)
+--       return
+--     end
+--   end
+-- end
 
-  -- If there is a previous buffer for the current window, switch to it
-  if PREV_BUFS[win] then
-    local prev_buf = PREV_BUFS[win]
-    vim.api.nvim_win_set_buf(win, prev_buf)
+local function pop_buf_from_stack(win, buf)
+  local bufs = PREV_BUFS[win]
+
+  if bufs and bufs[#bufs] == buf then
+    table.remove(bufs, #bufs)
   end
 
-  -- Update the previous buffer for the current window
-  PREV_BUFS[win] = buf
+  PREV_BUFS[win] = bufs
+end
+
+local function append_buffer_to_stack(win, buf)
+  local bufs = PREV_BUFS[win] or {}
+
+  table.insert(bufs, buf)
+  if #bufs > MAX_DEPTH then
+    table.remove(bufs, 1)
+  end
+
+  PREV_BUFS[win] = bufs
+end
+
+local function switch_to_prev_buf()
+  local win = vim.api.nvim_get_current_win()
+  local bufs = PREV_BUFS[win]
+  if bufs and #bufs > 0 then
+    local prev_buf = bufs[#bufs]
+    vim.api.nvim_win_set_buf(win, prev_buf)
+  else
+    print("No previous buffer to switch to for window", win, vim.inspect(bufs))
+  end
 end
 
 M.init = function()
@@ -24,7 +56,6 @@ M.init = function()
 
   local group = augroup("BufferSwitching", { clear = true })
 
-  -- Bind <C-Tab> to switch to the previous buffer
   vim.keymap.set(
     "n",
     "<C-Tab>",
@@ -32,29 +63,54 @@ M.init = function()
     { noremap = true, silent = true, desc = "Switch to previously opened buffer for this window" }
   )
 
-  -- Autocommand to clear previous buffer when a window is closed
-  autocmd("WinClosed", {
-    pattern = "*",
-    group = group,
-    desc = "Clear buffers tracked for given window",
-    callback = function()
-      PREV_BUFS[vim.api.nvim_get_current_win()] = nil
-    end,
-  })
+  local function get_window_for_buffer(buf)
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_buf(win) == buf then
+        return win
+      end
+    end
+    return nil
+  end
 
-  -- Autocommand to update previous buffer when a buffer is entered
   autocmd("BufLeave", {
     pattern = "*",
     group = group,
-    desc = "Update previous buffer when a buffer is entered",
-    callback = function()
-      local win = vim.api.nvim_get_current_win()
-      local buf = vim.api.nvim_win_get_buf(win)
-      if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, "buftype") == "" then
-        if buf ~= PREV_BUFS[win] then
-          PREV_BUFS[win] = buf
-        end
+    desc = "Append buffer to window stack when it is left",
+    callback = function(event)
+      local buf = event.buf
+      local win = get_window_for_buffer(buf)
+
+      if not win then
+        return
       end
+
+      if not vim.api.nvim_buf_is_loaded(buf) or vim.api.nvim_buf_get_option(buf, "buftype") ~= "" then
+        return
+      end
+
+      -- print("BufLeave =", buf, win)
+      append_buffer_to_stack(win, buf)
+    end,
+  })
+
+  autocmd("BufEnter", {
+    pattern = "*",
+    group = group,
+    desc = "Pop buffer from window stack when it is entered",
+    callback = function(event)
+      local buf = event.buf
+      local win = get_window_for_buffer(buf)
+
+      if not win then
+        return
+      end
+
+      if not vim.api.nvim_buf_is_loaded(buf) or vim.api.nvim_buf_get_option(buf, "buftype") ~= "" then
+        return
+      end
+
+      -- print("BufEnter =", buf, win)
+      pop_buf_from_stack(win, buf)
     end,
   })
 end
