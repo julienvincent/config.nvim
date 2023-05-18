0e0e990b7d3e074c8c84c2d4e9de04851d4ab3f6
Julien Vincent <m@julienvincent.io>
2023-05-18 10:58:34 +0100

    Improve git workflow

--- lua/julienvincent/plugins/git.lua
+++ lua/julienvincent/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "BufReadPost" },
     opts = {
       signs = {
         add = { text = "▎" },
@@ -12,7 +12,7 @@ return {
         untracked = { text = "▎" },
       },
       on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
+        local gs = require("gitsigns")
 
         local function map(mode, l, r, desc)
           vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
@@ -28,12 +28,36 @@ return {
         map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
         map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
         map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
-        map("n", "<leader>gd", gs.diffthis, "Diff file")
-        map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff file from ~")
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
       end,
     },
   },
 
-  { "TimUntersberger/neogit" },
+  {
+    "TimUntersberger/neogit",
+    opts = {
+      kind = "vsplit",
+      integrations = {
+        diffview = true
+      }
+    },
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" }
+    },
+    dependencies = {
+      { "sindrets/diffview.nvim" }
+    }
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
+      { "q", "<cmd>DiffviewClose<cr>", desc = "Close diff view" }
+    },
+    event = "VeryLazy",
+    opts = {
+      enhanced_diff_hl = true
+    }
+  }
 }
