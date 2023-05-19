6896551a2bff5648776739601606e3b8d9080a36
Julien Vincent <m@julienvincent.io>
2023-05-19 14:26:20 +0100

    Bind better telescope keys

--- lua/julienvincent/plugins/telescope.lua
+++ lua/julienvincent/plugins/telescope.lua
@@ -5,10 +5,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
       { "<leader><space>", "<cmd>Telescope find_files<cr>",                desc = "Find files" },
-      { "<leader>fa",      "<cmd>Telescope live_grep<cr>",                 desc = "Grep" },
-      { "<leader>ff",      "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Find in buffer" },
-      { "<leader>fgc",      "<cmd>Telescope git_commits<CR>",               desc = "commits" },
-      { "<leader>fgs",      "<cmd>Telescope git_status<CR>",                desc = "status" },
+      { "<leader>:",       "<cmd>Telescope commands<cr>",                  desc = "Grep" },
+      { "<leader>/",       "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Find in buffer" },
+      { "<leader>ff",      "<cmd>Telescope live_grep<cr>",                 desc = "Grep" },
+      { "<leader>fc",      "<cmd>Telescope git_commits<CR>",               desc = "commits" },
+      { "<leader>fs",      "<cmd>Telescope git_status<CR>",                desc = "status" },
     },
     opts = {
       pickers = {
