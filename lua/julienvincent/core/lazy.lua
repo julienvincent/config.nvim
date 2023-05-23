b67b46c21e5e8d16ca1e97d489312b9565dac1b2
Julien Vincent <m@julienvincent.io>
2023-05-23 01:25:38 +0100

    Cleanup lazy.vim config

--- lua/julienvincent/core/lazy.lua
+++ lua/julienvincent/core/lazy.lua
@@ -1,22 +1,18 @@
 -- Install lazy.nvim if not already installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Use a protected call so we don't error out on first use
-local ok, lazy = pcall(require, "lazy")
-if not ok then
-	return
-end
+local lazy = require("lazy")
 
 -- We have to set the leader keys here for lazy.nvim to work
 vim.g.mapleader = " "
@@ -29,4 +25,3 @@ lazy.setup("julienvincent.plugins", {
 })
 
 require("julienvincent.helpers.keys").map("n", "<leader>LL", lazy.show, "Show Lazy")
-
