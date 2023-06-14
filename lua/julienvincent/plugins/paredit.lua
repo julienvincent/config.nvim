4e20cb2ab198d6f06102dc49f68ba887b1a7f8f0
Julien Vincent <m@julienvincent.io>
2023-06-14 20:27:16 +0100

    Update paredit config to use remote repo

--- lua/julienvincent/plugins/paredit.lua
+++ lua/julienvincent/plugins/paredit.lua
@@ -1,26 +1,27 @@
 return {
   {
-    dir = "~/code/nvim-paredit",
+    "julienvincent/nvim-paredit",
+    -- dir = "~/code/nvim-paredit",
     ft = { "clojure" },
     config = function()
       local paredit = require("nvim-paredit")
       paredit.setup({
         use_default_keys = false,
         keys = {
-          ["<S-Right>"] = { paredit.api.slurpForwards, "Slurp" },
-          ["<S-Left>"] = { paredit.api.barfForwards, "Barf" },
+          ["<S-Right>"] = { paredit.api.slurp_forwards, "Slurp" },
+          ["<S-Left>"] = { paredit.api.barf_forwards, "Barf" },
 
-          ["<M-Left>"] = { paredit.api.dragElementBackwards, "Drag element left" },
-          ["<M-Right>"] = { paredit.api.dragElementForwards, "Drag element right" },
+          ["<M-Left>"] = { paredit.api.drag_element_backwards, "Drag element left" },
+          ["<M-Right>"] = { paredit.api.drag_element_forwards, "Drag element right" },
 
-          ["<M-S-Left>"] = { paredit.api.dragFormBackwards, "Drag form left" },
-          ["<M-S-Right>"] = { paredit.api.dragFormForwards, "Drag form right" },
+          ["<M-S-Left>"] = { paredit.api.drag_form_backwards, "Drag form left" },
+          ["<M-S-Right>"] = { paredit.api.drag_form_forwards, "Drag form right" },
 
-          ["<localleader>r"] = { paredit.api.raiseElement, "Raise element" },
-          ["<localleader>R"] = { paredit.api.raiseForm, "Raise form" },
+          ["<localleader>r"] = { paredit.api.raise_element, "Raise element" },
+          ["<localleader>R"] = { paredit.api.raise_form, "Raise form" },
 
-          ["E"] = { paredit.api.moveToNextElement, "Jump to next element" },
-          ["B"] = { paredit.api.moveToPrevElement, "Jump to previous element" },
+          ["E"] = { paredit.api.move_to_next_element, "Jump to next element" },
+          ["B"] = { paredit.api.move_to_prev_element, "Jump to previous element" },
         }
       })
     end
