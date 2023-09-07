b8e46849ffb89fe4cd3cc349f05ce14dc9377fba
Julien Vincent <m@julienvincent.io>
2023-09-07 15:13:56 +0100

    Allow closing HttpResult buffers with q

--- lua/julienvincent/core/autocmds.lua
+++ lua/julienvincent/core/autocmds.lua
@@ -44,6 +44,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "man",
     "spectre_panel",
     "tsplayground",
+    "HttpResult"
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
