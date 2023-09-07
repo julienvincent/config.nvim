ec0555da819ec940cdad9a40caf477f60e76af2e
Julien Vincent <m@julienvincent.io>
2023-09-07 15:13:40 +0100

    Only yank to system clipboard on y

--- lua/julienvincent/behaviours/yank-to-clipboard.lua
+++ lua/julienvincent/behaviours/yank-to-clipboard.lua
@@ -11,6 +11,9 @@ M.setup = function()
       if vim.tbl_contains(ignore_registers, vim.v.register) then
         return
       end
+      if vim.v.operator ~= "y" then
+        return
+      end
       vim.fn.setreg("+", vim.fn.getreg(vim.v.register, 1, 1))
     end,
   })
