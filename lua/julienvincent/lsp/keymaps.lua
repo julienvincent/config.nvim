e3fb2bf39c16cae4f92d17f7fb60f98fd50d69b9
Julien Vincent <m@julienvincent.io>
2023-09-19 22:40:34 +0100

    Split lsp components into modules

---
+++ lua/julienvincent/lsp/keymaps.lua
@@ -0,0 +1,19 @@
+return {
+  g = {
+    d = { ":Telescope lsp_definitions<cr>", "Go to Definition" },
+    i = { ":Telescope lsp_implementations<cr>", "Go to Impementations" },
+    r = { ":Telescope lsp_references<cr>", "Symbol References" },
+    t = { ":Telescope lsp_type_definitions<cr>", "Type Definitions" },
+  },
+  t = { ":Telescope lsp_type_definitions<cr>", "Type Definition" },
+  ["<leader>"] = {
+    l = {
+      f = { vim.lsp.buf.format, "Format" },
+      r = { vim.lsp.buf.rename, "Rename" },
+      R = { ":LspRestart<cr>", "Lsp Restart" },
+      a = { vim.lsp.buf.code_action, "Code Action" },
+      I = { ":LspInfo<cr>", "Lsp Info" },
+    },
+  },
+  K = { vim.lsp.buf.hover, "Hover doc" },
+}
---
+++ lua/julienvincent/lsp/servers.lua
@@ -0,0 +1,68 @@
+local utils = require("julienvincent.lsp.utils")
+
+return {
+  clojure_lsp = {
+    root_dir = utils.find_furthest_root({ "deps.edn", "bb.edn", "project.clj", "shadow-cljs.edn" }),
+    single_file_support = true,
+    init_options = {
+      codeLens = true,
+    },
+
+    before_init = function(params)
+      params.workDoneToken = "enable-progress"
+    end,
+  },
+  yamlls = {
+    settings = {
+      yaml = {
+        schemas = {
+          "https://json.schemastore.org/github-workflow.json",
+          ".github/workflows/*",
+        },
+      },
+    },
+  },
+  tsserver = {},
+  jdtls = {
+    single_file_support = true,
+    settings = {
+      java = {
+        signatureHelp = { enabled = true },
+        contentProvider = { preferred = "fernflower" },
+      },
+    },
+  },
+  jsonls = {},
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        files = {
+          excludeDirs = { ".embuild", "target", ".git" },
+        },
+      },
+    },
+  },
+  lua_ls = {
+    settings = {
+      Lua = {
+        format = {
+          enable = false,
+          defaultConfig = {
+            indent_style = "space",
+            indent_size = "2",
+          },
+        },
+        runtime = {
+          version = "LuaJIT",
+        },
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          checkThirdParty = false,
+        },
+        telemetry = { enable = false },
+      },
+    },
+  },
+}
