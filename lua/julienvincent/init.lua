64f5e85cbca28fd8fa0ac7173008b635f2d8e23a
Julien Vincent <m@julienvincent.io>
2023-09-19 16:27:12 +0100

    Remove tab-name behaviour

--- lua/julienvincent/init.lua
+++ lua/julienvincent/init.lua
@@ -8,6 +8,5 @@ require("julienvincent.lang.http").setup()
 
 require("julienvincent.behaviours.buffer-switching").setup()
 require("julienvincent.behaviours.yank-to-clipboard").setup()
-require("julienvincent.behaviours.tab-name").setup()
 require("julienvincent.behaviours.auto-save").setup()
 require("julienvincent.behaviours.undo").setup()
--- lua/julienvincent/plugins/bufferline.lua
+++ lua/julienvincent/plugins/bufferline.lua
@@ -1,5 +1,3 @@
-local tab_name = require("julienvincent.behaviours.tab-name")
-
 return {
   {
     "akinsho/bufferline.nvim",
@@ -10,10 +8,6 @@ return {
       options = {
         mode = "tabs",
         themable = true,
-        name_formatter = function(buf)
-          local tabnr = vim.api.nvim_tabpage_get_number(buf.tabnr)
-          return tab_name.get_name(tabnr)
-        end,
         separator_style = "thin",
         always_show_bufferline = false,
       },
