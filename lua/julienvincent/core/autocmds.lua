79265613fbef41c0dcf948761ac3a4eac8879d97
Julien Vincent <m@julienvincent.io>
2023-10-29 22:08:41 +0000

    Enable linebreak for markdown files

--- lua/julienvincent/core/autocmds.lua
+++ lua/julienvincent/core/autocmds.lua
@@ -31,6 +31,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown" },
   callback = function()
     vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
     vim.opt_local.shiftwidth = 2
   end,
 })
