cad47c3d8a1e6d6fd95b2923bf05aafc06f6002a
Julien Vincent <m@julienvincent.io>
2023-11-08 12:29:16 +0100

    Implement custom log buffer scroll for conjure

--- lua/julienvincent/plugins/conjure.lua
+++ lua/julienvincent/plugins/conjure.lua
@@ -25,6 +25,31 @@ local keymaps = {
   },
 }
 
+local function get_win_by_buf(bufnr)
+  local wins = vim.api.nvim_list_wins()
+  for _, win in ipairs(wins) do
+    if vim.api.nvim_win_get_buf(win) == bufnr then
+      return win
+    end
+  end
+  return nil
+end
+
+local function scroll_conjure_log_to_bottom()
+  local bufs = vim.api.nvim_list_bufs()
+  for _, bufnr in ipairs(bufs) do
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    if name:find("conjure%-log%-") then
+      local win = get_win_by_buf(bufnr)
+      if not win then
+        return
+      end
+      local line_count = vim.api.nvim_buf_line_count(bufnr)
+      vim.api.nvim_win_set_cursor(win, { line_count, 0 })
+    end
+  end
+end
+
 return {
   {
     "Olical/conjure",
@@ -46,6 +71,7 @@ return {
       vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
 
       vim.g["conjure#mapping#doc_word"] = false
+      vim.g["conjure#mapping#log_jump_to_latest"] = false
 
       vim.g["conjure#client#clojure#nrepl#mapping#disconnect"] = false
       vim.g["conjure#client#clojure#nrepl#mapping#connect_port_file"] = false
@@ -94,6 +120,21 @@ return {
         },
       }, {
         prefix = "<leader>",
+      })
+
+      -- I register my own implementation of the `log_jump_to_latest` '<localleader>ll' mapping
+      -- as the default conjure mapping jumpts to the start of the last eval'd expression instead
+      -- of the end. This can be very annoying as it does not re-enable auto-scroll.
+      wk.register({
+        l = {
+          name = "Conjure Log",
+          l = {
+            scroll_conjure_log_to_bottom,
+            "Scroll conjure log to bottom",
+          },
+        },
+      }, {
+        prefix = "<localleader>",
       })
 
       vim.api.nvim_create_autocmd("FileType", {
