8e23103abcc33d48c6fd333057ff50c33f62c028
Julien Vincent <m@julienvincent.io>
2023-11-17 08:45:35 +0800

    Replace multi-cursor plugin

--- lua/julienvincent/plugins/multi-cursor.lua
+++ lua/julienvincent/plugins/multi-cursor.lua
@@ -1,7 +1,13 @@
 return {
   {
-    "mg979/vim-visual-multi",
-    event = "BufReadPost",
-    enabled = false,
+    "brenton-leighton/multiple-cursors.nvim",
+    config = true,
+    keys = {
+      { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i" } },
+      { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>" },
+      { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i" } },
+      { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>" },
+      { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" } },
+    },
   },
 }
