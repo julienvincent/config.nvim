0d0f47cc29014525d7806b61982c52c3298a9432
Julien Vincent <m@julienvincent.io>
2024-01-16 16:51:45 +0000

    Use biome formating in projects with a biome.json

--- lua/julienvincent/plugins/formatting.lua
+++ lua/julienvincent/plugins/formatting.lua
@@ -1,3 +1,7 @@
+local function file_exists(file)
+  return vim.loop.fs_stat(vim.fn.getcwd() .. "/" .. file) ~= nil
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -5,13 +9,18 @@ return {
     config = function()
       local conform = require("conform")
 
-      local js_formatter = { "prettierd", "prettier" }
+      local js_formatter = function()
+        if file_exists("biome.json") and not file_exists(".prettierrc") then
+          return { "biome" }
+        end
+        return { { "prettierd", "prettier" } }
+      end
 
       conform.setup({
         formatters_by_ft = {
-          javascript = { js_formatter },
-          typescript = { js_formatter },
-          typescriptreact = { js_formatter },
+          javascript = js_formatter,
+          typescript = js_formatter,
+          typescriptreact = js_formatter,
 
           lua = { "stylua" },
           just = { "just" },
