be1dcff94d0406e7866e1ca0eeaaee03fe8c89ee
Julien Vincent <m@julienvincent.io>
2024-02-06 01:23:04 +0000

    Format lsp/servers.lua

--- lua/julienvincent/lsp/servers.lua
+++ lua/julienvincent/lsp/servers.lua
@@ -4,7 +4,10 @@ local M = {}
 
 M.servers = {
   clojure_lsp = {
-    root_dir = utils.find_furthest_root({ "deps.edn", "bb.edn", "project.clj", "shadow-cljs.edn" }, utils.fallback_fn_tmp_dir),
+    root_dir = utils.find_furthest_root(
+      { "deps.edn", "bb.edn", "project.clj", "shadow-cljs.edn" },
+      utils.fallback_fn_tmp_dir
+    ),
     single_file_support = true,
     init_options = {
       codeLens = true,
