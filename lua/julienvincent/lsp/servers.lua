98eed30e283fb005e85909b86276de979241f63d
Julien Vincent <m@julienvincent.io>
2024-05-22 13:18:23 +0100

    Always use kmono for clojure-lsp cp command

--- lua/julienvincent/lsp/servers.lua
+++ lua/julienvincent/lsp/servers.lua
@@ -25,27 +25,15 @@ M.servers = {
       single_file_support = true,
       init_options = {
         codeLens = true,
+        ["project-specs"] = {
+          {
+            ["project-path"] = "deps.edn",
+            ["classpath-cmd"] = { "kmono", "cp" },
+          },
+        },
       },
 
       before_init = function(params)
-        local file = io.open(params.rootPath .. "/deps.edn", "r")
-        local deps_content
-        if file then
-          deps_content = file:read("*all")
-          file:close()
-        end
-
-        -- If this is a kmono workspace then we configure clojure-lsp classpath using
-        -- kmono cp
-        if deps_content and string.find(deps_content, ":kmono/workspace") then
-          params.initializationOptions["project-specs"] = {
-            {
-              ["project-path"] = "deps.edn",
-              ["classpath-cmd"] = { "kmono", "cp", "-P", ":*/test" },
-            },
-          }
-        end
-
         params.workDoneToken = "enable-progress"
       end,
     }
