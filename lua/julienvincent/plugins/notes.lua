cec21a65d756fa5b3844b34ec67eed0be49f50d7
Julien Vincent <m@julienvincent.io>
2024-05-22 13:18:23 +0100

    Add new plugin global-note

---
+++ lua/julienvincent/plugins/notes.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "backdround/global-note.nvim",
+    event = "VeryLazy",
+    config = function()
+      local global_note = require("global-note")
+      global_note.setup({
+        post_open = function(buffer_id)
+          vim.keymap.set("n", "q", global_note.toggle_note, {
+            desc = "Close note",
+            buffer = buffer_id,
+          })
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>N", global_note.toggle_note, {
+        desc = "Toggle global note",
+      })
+    end,
+  },
+}
