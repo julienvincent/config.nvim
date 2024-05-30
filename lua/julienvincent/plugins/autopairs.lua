cd961fa60ddfa5c69bb8ab593598fa28cf6d472e
Julien Vincent <m@julienvincent.io>
2024-05-30 11:39:08 +0100

    Disable <> autopairs rule

--- lua/julienvincent/plugins/autopairs.lua
+++ lua/julienvincent/plugins/autopairs.lua
@@ -12,9 +12,33 @@ return {
       })
 
       pairs.get_rules("`")[1].not_filetypes = { "clojure" }
-      pairs.get_rules("'")[1].not_filetypes = { "clojure" }
+      pairs.get_rules("'")[1].not_filetypes = { "clojure", "rust" }
 
-      pairs.add_rule(Rule("<", ">", "typescript"))
+      -- local type_def_rule = Rule("<", ">", { "typescript", "rust" })
+      --   :with_del(function(opts)
+      --     -- This deletes the pair if `<` is pressed twice
+      --     local line = opts.line
+      --     local col = opts.col
+      --     if col > 1 and line:sub(col - 1, col - 1) == "<" then
+      --       return true
+      --     end
+      --     return false
+      --   end)
+      --   :with_move(function(opts)
+      --     return opts.char == ">"
+      --   end)
+      --   :with_pair(function(opts)
+      --     -- This checks if the character before the cursor is also `<`
+      --     local line = opts.line
+      --     local col = opts.col
+      --     return not (col > 1 and line:sub(col - 1, col - 1) == "<")
+      --   end)
+      --
+      -- pairs.add_rule(type_def_rule)
+
+      -- pairs.add_rule(Rule("<", ">", { "typescript", "rust" }):with_move(function(opts)
+      --   return opts.char == ">"
+      -- end))
     end,
   },
 }
