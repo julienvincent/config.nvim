8210b1a8ffc75bc104ca8bf77647e1dd620c4a4e
Julien Vincent <m@julienvincent.io>
2024-06-26 23:43:11 +0100

    Replace rest.nvim with kulala

--- lua/julienvincent/modules/lang/http.lua
+++ lua/julienvincent/modules/lang/http.lua
@@ -1,19 +1,5 @@
 local M = {}
 
-local function select_env()
-  vim.ui.input({
-    prompt = "Select env file",
-    default = ".env.",
-    kind = "center",
-  }, function(value)
-    if not value then
-      return
-    end
-
-    vim.cmd.RestSelectEnv(value)
-  end)
-end
-
 function M.setup()
   vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
     pattern = { "*.http" },
@@ -21,13 +7,6 @@ function M.setup()
     callback = function(event)
       vim.api.nvim_buf_set_option(event.buf, "filetype", "http")
       vim.bo.commentstring = "#%s"
-
-      local map = vim.keymap.set
-
-      map("n", "<localleader>rr", "<Plug>RestNvim<Cr>", { desc = "Run HTTP request", buffer = event.buf })
-      map("n", "<localleader>rp", "<Plug>RestNvimPreview<Cr>", { desc = "Preview HTTP request", buffer = event.buf })
-      map("n", "<localleader>rl", "<Plug>RestNvimLast<Cr>", { desc = "Rerun last request", buffer = event.buf })
-      map("n", "<localleader>rs", select_env, { desc = "Select env", buffer = event.buf })
     end,
   })
 end
--- lua/julienvincent/plugins/rest.lua
+++ lua/julienvincent/plugins/rest.lua
@@ -1,13 +1,28 @@
 return {
   {
-    "rest-nvim/rest.nvim",
-    version = "1.x",
-    ft = "http",
+    "mistweaverco/kulala.nvim",
     config = function()
-      require("rest-nvim").setup({
-        highlight = {
-          enabled = false,
-        },
+      local kulala = require("kulala")
+
+      kulala.setup({
+        default_view = "body",
+        default_env = "local",
+        debug = false,
+      })
+
+      vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+        pattern = { "*.http" },
+        desc = "Set the filetype of .http files",
+        callback = function()
+          vim.keymap.set("n", "<localleader>rr", function()
+            print("but why")
+            kulala.run()
+          end, { desc = "Execute request under cursor" })
+
+          vim.keymap.set("n", "<localleader>rs", function()
+            kulala.set_selected_env()
+          end, { desc = "Select request environment" })
+        end,
       })
     end,
   },
