d2dadab81866de38c79b4a5498536b1065956cf5
Julien Vincent <m@julienvincent.io>
2024-07-10 10:46:36 +0100

    Update bufferline

--- lua/julienvincent/plugins/bufferline.lua
+++ lua/julienvincent/plugins/bufferline.lua
@@ -2,8 +2,6 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    version = "*",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
         mode = "tabs",
