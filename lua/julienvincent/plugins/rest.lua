b3e1caff4e73cee57b43707ae7df622247ba686a
Julien Vincent <m@julienvincent.io>
2024-09-19 21:25:16 +0100

    Configure rest keybinds on first load

--- lua/julienvincent/plugins/rest.lua
+++ lua/julienvincent/plugins/rest.lua
@@ -11,19 +11,29 @@ return {
         debug = false,
       })
 
+      local function setup_keybinds(event)
+        vim.keymap.set("n", "<localleader>rr", function()
+          kulala.run()
+        end, { desc = "Execute request under cursor", buffer = event.buf })
+
+        vim.keymap.set("n", "<localleader>rs", function()
+          kulala.set_selected_env()
+        end, { desc = "Select request environment", buffer = event.buf })
+      end
+
       vim.api.nvim_create_autocmd({ "FileType", "BufNewFile", "BufRead" }, {
         pattern = { "*.http" },
         desc = "Set the filetype of .http files",
-        callback = function()
-          vim.keymap.set("n", "<localleader>rr", function()
-            kulala.run()
-          end, { desc = "Execute request under cursor" })
-
-          vim.keymap.set("n", "<localleader>rs", function()
-            kulala.set_selected_env()
-          end, { desc = "Select request environment" })
-        end,
+        callback = setup_keybinds,
       })
+
+      local filetype = vim.api.nvim_get_option_value("ft", {
+        buf = 0,
+      })
+
+      if filetype == "http" then
+        setup_keybinds({ buf = 0 })
+      end
     end,
   },
 }
