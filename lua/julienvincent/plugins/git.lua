80d36de619732d5b6ce573b38227b75b33b82246
Julien Vincent <m@julienvincent.io>
2024-09-19 21:25:16 +0100

    Merge jujutsu config into git plugin module

--- lua/julienvincent/plugins/git.lua
+++ lua/julienvincent/plugins/git.lua
@@ -1,4 +1,15 @@
 return {
+  { "avm99963/vim-jjdescription" },
+
+  {
+    "julienvincent/hunk.nvim",
+    cmd = { "DiffEditor" },
+    config = function()
+      local hunk = require("hunk")
+      hunk.setup()
+    end,
+  },
+
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost" },
