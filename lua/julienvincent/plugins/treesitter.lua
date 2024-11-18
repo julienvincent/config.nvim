dd5e5f7139ce34e3e6a46543f3680017df573bde
Julien Vincent <m@julienvincent.io>
2024-11-18 21:00:08 +0000

    Disable treesitter highlighting in large files

--- lua/julienvincent/plugins/treesitter.lua
+++ lua/julienvincent/plugins/treesitter.lua
@@ -10,7 +10,14 @@ return {
         ignore_install = {},
         sync_install = false,
         auto_install = false,
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          disable = function(_, bufnr)
+            local buf_name = vim.api.nvim_buf_get_name(bufnr)
+            local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+            return file_size > 256 * 1024
+          end,
+        },
         indent = { enable = true },
         ensure_installed = {
           "nim",
