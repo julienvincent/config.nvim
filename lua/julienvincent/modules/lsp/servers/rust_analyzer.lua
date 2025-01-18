25b7fba282a730f630bf9e4e73d1203a488c3554
Julien Vincent <m@julienvincent.io>
2025-01-18 12:43:36 +0000

    Stop at rust workspaces during lsp root selection

--- lua/julienvincent/modules/lsp/servers/rust_analyzer.lua
+++ lua/julienvincent/modules/lsp/servers/rust_analyzer.lua
@@ -1,6 +1,23 @@
 local mason = require("julienvincent.modules.core.mason")
 local fs = require("julienvincent.modules.lsp.utils.fs")
 
+local function is_workspace_root(path)
+  local file_path = path .. "/Cargo.toml"
+  local file = io.open(file_path, "r")
+  if not file then
+    return false
+  end
+
+  local content = file:read("*all")
+  file:close()
+
+  if string.find(content, "%[workspace]") then
+    return true
+  else
+    return false
+  end
+end
+
 return {
   name = "rust-analyzer",
   filetypes = { "rust", "edn" },
@@ -8,7 +25,7 @@ return {
   cmd = mason.command("rust-analyzer"),
   root_dir = fs.find_furthest_root(
     {
-      "Cargo.toml",
+      { "Cargo.toml", is_root = is_workspace_root },
     },
     fs.find_closest_root({
       ".git",
