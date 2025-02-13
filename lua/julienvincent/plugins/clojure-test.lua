a97c4a94032af89f13b8df5c2d8e0b48183e22d2
Julien Vincent <m@julienvincent.io>
2025-02-13 13:44:15 +0000

    Update clojure-test.nvim

--- lua/julienvincent/plugins/clojure-test.lua
+++ lua/julienvincent/plugins/clojure-test.lua
@@ -26,6 +26,8 @@ return {
       vim.keymap.set("n", "<localleader>tr", api.run_tests, { desc = "Run tests" })
       vim.keymap.set("n", "<localleader>tn", api.run_tests_in_ns, { desc = "Run tests in a namespace" })
       vim.keymap.set("n", "<localleader>tp", api.rerun_previous, { desc = "Rerun the most recently run tests" })
+      vim.keymap.set("n", "<localleader>tf", api.rerun_failed, { desc = "Rerun failed tests from the previous run" })
+      vim.keymap.set("n", "<localleader>to", api.open_last_report, { desc = "Re-open the previous report" })
 
       vim.keymap.set("n", "<localleader>!", function()
         api.analyze_exception("*e")
