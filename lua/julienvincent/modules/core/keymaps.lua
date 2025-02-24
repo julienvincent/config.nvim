34b533beb075b2d60c27d363b64a44dca1d8d1a9
Julien Vincent <m@julienvincent.io>
2025-02-24 10:58:08 +0000

    Use vim.keymap.set directly

--- lua/julienvincent/modules/core/keymaps.lua
+++ lua/julienvincent/modules/core/keymaps.lua
@@ -4,32 +4,32 @@ M.setup = function()
   local saving = require("julienvincent.modules.core.auto-save")
   local lazy = require("lazy")
 
-  local map = vim.keymap.set
-
-  map("n", "<leader>qq", function()
+  vim.keymap.set("n", "<leader>qq", function()
     saving.write_all_buffers()
     vim.api.nvim_command("qa")
   end, { desc = "Save and quit" })
 
-  map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+  vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", {
+    desc = "Escape and clear hlsearch",
+  })
 
   -- tabs
-  map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "Next Tab" })
-  map("n", "<leader><tab><Right>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-  map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-  map("n", "<leader><tab><Left>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+  vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "Next Tab" })
+  vim.keymap.set("n", "<leader><tab><Right>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+  vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+  vim.keymap.set("n", "<leader><tab><Left>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
-  vim.api.nvim_set_keymap("n", "<S-Down>", "<Nop>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<S-Up>", "<Nop>", { noremap = true, silent = true })
+  vim.keymap.set("n", "<S-Down>", "<Nop>", { noremap = true, silent = true })
+  vim.keymap.set("n", "<S-Up>", "<Nop>", { noremap = true, silent = true })
 
-  map("n", "<leader>LL", lazy.show, { desc = "Show Lazy" })
+  vim.keymap.set("n", "<leader>LL", lazy.show, { desc = "Show Lazy" })
 
-  map("v", "<localleader>s", '"zy:%s/<C-r>z//g<Left><Left>', {
+  vim.keymap.set("v", "<localleader>s", '"zy:%s/<C-r>z//g<Left><Left>', {
     noremap = true,
     desc = "Replace selection",
   })
 
-  map("n", "gx", function()
+  vim.keymap.set("n", "gx", function()
     local uri = vim.fn.expand("<cfile>")
     vim.fn.jobstart({ "open", uri }, {
       detach = true,
@@ -39,7 +39,7 @@ M.setup = function()
     desc = "Open URI under cursor",
   })
 
-  map("n", "yc", "yy<cmd>normal gcc<CR>p", {
+  vim.keymap.set("n", "yc", "yy<cmd>normal gcc<CR>p", {
     desc = "Duplicate line below, commenting first",
   })
 end
