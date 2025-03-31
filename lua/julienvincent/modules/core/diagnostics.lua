28fddea4eb2489099c4376fb11c6a174e3796aa4
Julien Vincent <m@julienvincent.io>
2025-03-31 10:16:24 +0100

    Use virtual_lines for default diagnostics view

--- lua/julienvincent/modules/core/diagnostics.lua
+++ lua/julienvincent/modules/core/diagnostics.lua
@@ -11,5 +11,34 @@ return {
         },
       },
     })
+
+    local virtual_lines_toggled = false
+    vim.keymap.set("n", "<localleader>d", function()
+      if virtual_lines_toggled then
+        vim.diagnostic.config({
+          virtual_lines = false,
+        })
+      else
+        vim.diagnostic.config({
+          virtual_lines = {
+            current_line = true,
+          },
+        })
+      end
+
+      virtual_lines_toggled = not virtual_lines_toggled
+    end)
+
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      desc = "Turn off virtual lines on cursor moved",
+      callback = function(_)
+        if virtual_lines_toggled then
+          vim.diagnostic.config({
+            virtual_lines = false,
+          })
+          virtual_lines_toggled = false
+        end
+      end,
+    })
   end,
 }
