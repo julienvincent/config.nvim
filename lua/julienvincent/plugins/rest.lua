d6e3225efad47bcae092e3f11701f5d2e73ad8e5
Julien Vincent <m@julienvincent.io>
2025-04-12 17:24:29 +0100

    Setup additional kulala keybindings

--- lua/julienvincent/plugins/rest.lua
+++ lua/julienvincent/plugins/rest.lua
@@ -4,11 +4,21 @@ return {
     ft = { "http" },
     config = function()
       local kulala = require("kulala")
+      local ui = require("kulala.ui")
 
       kulala.setup({
         default_view = "body",
         default_env = "local",
         debug = false,
+
+        kulala_keymaps = {
+          ["Show headers and body"] = {
+            "<localleader>a",
+            function()
+              require("kulala.ui").show_headers_body()
+            end,
+          },
+        },
       })
 
       local function setup_keybinds(event)
@@ -16,9 +26,33 @@ return {
           kulala.run()
         end, { desc = "Execute request under cursor", buffer = event.buf })
 
+        vim.keymap.set("n", "<localleader>rp", function()
+          kulala.replay()
+        end, { desc = "Re-run the last executed request", buffer = event.buf })
+
         vim.keymap.set("n", "<localleader>rs", function()
           kulala.set_selected_env()
         end, { desc = "Select request environment", buffer = event.buf })
+
+        vim.keymap.set("n", "[r", function()
+          ui.show_previous()
+        end, { desc = "Prev request", buffer = event.buf })
+
+        vim.keymap.set("n", "]r", function()
+          ui.show_next()
+        end, { desc = "Next request", buffer = event.buf })
+
+        vim.keymap.set("n", "[[", function()
+          kulala.jump_prev()
+        end, { desc = "Prev request", buffer = event.buf })
+
+        vim.keymap.set("n", "]]", function()
+          kulala.jump_next()
+        end, { desc = "Next request", buffer = event.buf })
+
+        vim.keymap.set("n", "<localleader>rf", function()
+          kulala.search()
+        end, { desc = "Search request history", buffer = event.buf })
       end
 
       vim.api.nvim_create_autocmd({ "FileType", "BufNewFile", "BufRead" }, {
