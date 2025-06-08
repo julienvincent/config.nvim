6c370c05984fd948730785e41571a747996f5ee8
Julien Vincent <m@julienvincent.io>
2025-06-08 23:22:01 +0100

    Load vim experimental ui last

--- lua/julienvincent/init.lua
+++ lua/julienvincent/init.lua
@@ -1,7 +1,7 @@
--- Experimental replacement to the vim messages interface
-require("vim._extui").enable({})
-
 require("julienvincent.lazy")
 require("julienvincent.options")
 
 require("julienvincent.modules").setup()
+
+-- Experimental replacement to the vim messages interface
+require("vim._extui").enable({})
