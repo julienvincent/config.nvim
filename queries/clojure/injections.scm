d4ac5ee4f3576bc168a4d6214e1b328b39c7b7a2
Julien Vincent <m@julienvincent.io>
2025-07-21 14:59:13 +0100

    Replace metadata-based sql injection with regex
    This uses regex to match if a string contains SQL which is then in turn
    used to apply the sql language injection to the string.
    
    This works much better than the previous ^:sql metadata approach as this
    doesn't require adding anything to source-code and works for ANY string
    in source that might contain sql.

--- queries/clojure/injections.scm
+++ queries/clojure/injections.scm
@@ -56,58 +56,7 @@
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "markdown"))
 
-;; This example is for defining a clojure SQL language injection for clojure forms that look like:
-;;
-;; (def ^:sql query "SELECT * FROM foo;")
-;;
-;; However this does not work due to the `str_lit` node including the wrapping '"' chars. This would
-;; require a patch to the clojure TreeSitter grammer to further break the str_lit node into
-;; sub-nodes.
-((list_lit
-  (sym_lit) @def-type
-
-  (sym_lit
-    (meta_lit 
-      (kwd_lit
-        (kwd_name) @var.meta)))
-
-  (str_lit) @injection.content)
-
-  (#eq? @def-type "def")
-  (#eq? @var.meta "sql")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-((list_lit
-  .
-
-  (sym_lit) @fn-name
-
-  (_)
-
-  [
-    (str_lit) @injection.content
-
-    (vec_lit
-      (str_lit) @injection.content)
-  ])
-
-  (#any-of? @fn-name "jdbc/execute!" "jdbc/execute-one!" "jdbc/plan")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-((list_lit
-  .
-
-  (sym_lit) @fn-name
-
-  (_)
-
-  (_)
-
-  (vec_lit
-    (str_lit) @injection.content))
-
-  (#match? @fn-name "\/(select!|select-one!)")
+((str_lit)  @injection.content
+  (#match? @injection.content "^\"(SELECT |CREATE |ALTER |UPDATE |DROP |INSERT )")
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "sql"))
