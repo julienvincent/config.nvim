102f2590bae80a2dbbcb935a70c78856ff033036
Julien Vincent <m@julienvincent.io>
2025-08-07 14:35:25 +0100

    Add DELETE to sql injection

--- queries/clojure/injections.scm
+++ queries/clojure/injections.scm
@@ -57,6 +57,6 @@
   (#set! injection.language "markdown"))
 
 ((str_lit)  @injection.content
-  (#match? @injection.content "^\"(SELECT|CREATE|ALTER|UPDATE|DROP|INSERT|WITH)")
+  (#match? @injection.content "^\"(SELECT|CREATE|DELETE|ALTER|UPDATE|DROP|INSERT|WITH)")
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "sql"))
