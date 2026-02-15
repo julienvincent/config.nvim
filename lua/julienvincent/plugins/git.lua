e5d56dc9720b17a9c0b0828bcd6c3cf7c44b78ad
Julien Vincent <m@julienvincent.io>
2026-02-15 21:05:47 +0000

    Change git sign gutting characters

--- lua/julienvincent/plugins/git.lua
+++ lua/julienvincent/plugins/git.lua
@@ -46,12 +46,12 @@ return {
     event = { "BufReadPost" },
     opts = {
       signs = {
-        add = { text = "❙" },
-        change = { text = "❙" },
+        add = { text = "|" },
+        change = { text = "|" },
         delete = { text = "" },
         topdelete = { text = "" },
-        changedelete = { text = "❙" },
-        untracked = { text = "❙" },
+        changedelete = { text = "|" },
+        untracked = { text = "|" },
       },
       attach_to_untracked = true,
       on_attach = function(buffer)
